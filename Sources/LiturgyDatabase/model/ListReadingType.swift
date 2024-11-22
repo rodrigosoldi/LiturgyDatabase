a89a83fb991fba749a692ea2911ca3d93fb8be33
soldi <rodrigosoldi@icloud.com>
2024-11-22 09:55:00 -0300

    listreadingtype implements identifiable

--- Sources/LiturgyDatabase/model/ListReadingType.swift
+++ Sources/LiturgyDatabase/model/ListReadingType.swift
@@ -5,11 +5,26 @@
 //  Created by Rodrigo Soldi on 19/11/24.
 //
 
-public enum ListReadingType {
+import Foundation
+
+public enum ListReadingType: Identifiable {
     case reading(Reading)
     case psalm(Psalm)
     case gospel(Gospel)
     case sequence(Sequence)
+    
+    public var id: UUID {
+        switch self {
+        case .reading(let reading):
+            return reading.id
+        case .psalm(let psalm):
+            return psalm.id
+        case .gospel(let gospel):
+            return gospel.id
+        case .sequence(let sequence):
+            return sequence.id
+        }
+    }
     
     func contains(_ text: String) -> Bool {
         let _text = text.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
