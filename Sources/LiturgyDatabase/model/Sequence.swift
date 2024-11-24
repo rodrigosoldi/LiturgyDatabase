6d151485d31bf3878caf8b3751d4838afd103799
soldi <rodrigosoldi@icloud.com>
2024-11-24 13:45:54 -0300

    sequence public init

--- Sources/LiturgyDatabase/model/Sequence.swift
+++ Sources/LiturgyDatabase/model/Sequence.swift
@@ -13,6 +13,13 @@ public struct Sequence: Identifiable {
     public let text: String
     public let observation: String?
     
+    init(id: UUID, title: String?, text: String, observation: String?) {
+        self.id = id
+        self.title = title
+        self.text = text
+        self.observation = observation
+    }
+    
     func contains(_ text: String) -> Bool {
         let _text = text.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         return id.uuidString.contains(_text) ||
